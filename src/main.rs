078603158cb0edda64c54c6903620dbc439415b1
Chris Mounce <christophermounce@gmail.com>
2016-06-08 22:57:47 -0700

    Optimize polynomial math further
    This change saves about another 9-10ms per frame.
    
    This commit also changes the names from fsin() and fcos() to ywave() and
    xwave(), because they no longer have a period of 2*PI.

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,66 @@ use sdl2::render::Renderer;
 use sdl2::render::Texture;
 use std::cmp;
 use std::f32;
-use std::f32::consts::PI;
 use std::time::SystemTime;
 
 const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
 
 trait FastMath<F> {
-    fn fcos(&self) -> F;
-    fn fsin(&self) -> F;
+    fn xwave(&self) -> F;
+    fn ywave(&self) -> F;
 }
 
 impl FastMath<f32> for f32 {
-    fn fsin(&self) -> f32 {
-        let x = self/PI;
-        let fract = if x > 0.0 { x.fract() } else { 1.0 + x.fract() };
-        let curve = (-4.0*fract + 4.0)*fract; // f(x) = -4x^2 + 4x, upside-down parabola
-        if (x.floor() as u32) & 1 == 1 {
-            return -curve;
-        } else {
-            return curve;
-        }
+    // Like sin(), except its period is 10 instead of 2*PI
+    fn ywave(&self) -> f32 {
+        // x loops over the range (-0.5, 0.5)
+        let adj = self/10.0;
+        let x = adj - adj.floor() - 0.5;
+
+        // f(x) = 16x^2 - 8x, a parabola centered on the interval (0.0, 0.5).
+        // Having one x and one x.abs() flips the parabola when x is negative.
+        x * x.abs().mul_add(16.0, -8.0)
     }
 
-    fn fcos(&self) -> f32 {
-        return (self + 0.5*PI).fsin();
+    // Like cos(), except its period is 10 instead of 2*PI
+    fn xwave(&self) -> f32 {
+        return (self + 2.5).ywave();
     }
+}
+
+macro_rules! assert_feq {
+    ($expected:expr, $actual:expr) => (
+        {
+            let expected:f32 = $expected;
+            let actual:f32 = $actual;
+            assert!(expected.abs_sub(actual) < 0.01, "assertion failed: {} != {}", expected, actual);
+        }
+    );
+}
+
+#[test]
+fn test_ywave() {
+    // Test area around 0
+    assert_feq!((-0.5).ywave(), 0.0);
+    assert_feq!((-0.25).ywave(), -1.0);
+    assert_feq!((0.0).ywave(), 0.0);
+    assert_feq!((0.25).ywave(), 1.0);
+    assert_feq!((0.5).ywave(), 0.0);
+
+    // Test area further away
+    assert_feq!((8.0).ywave(), 0.0);
+    assert_feq!((8.25).ywave(), 1.0);
+    assert_feq!((8.5).ywave(), 0.0);
+    assert_feq!((8.75).ywave(), -1.0);
+    assert_feq!((9.0).ywave(), 0.0);
+
+    // Test area further into the negatives
+    assert_feq!((-8.0).ywave(), 0.0);
+    assert_feq!((-7.75).ywave(), 1.0);
+    assert_feq!((-7.5).ywave(), 0.0);
+    assert_feq!((-7.25).ywave(), -1.0);
+    assert_feq!((-7.0).ywave(), 0.0);
 }
 
 struct Plasma {
@@ -58,11 +92,11 @@ impl Plasma {
 
     fn calculate_color(&mut self, x: f32, y: f32) -> (u8, u8, u8) {
         let mut value = 0.0;
-        value += (x/23.0 + self.time).fcos();
-        value += (x/13.0 + (y/17.0)*(self.time/2.0).fcos() ).fcos();
-        let dx = (self.time/1.9).fcos()*200.0 + (WIDTH as f32)/2.0 - x;
-        let dy = (self.time/3.1).fsin()*150.0 + (HEIGHT as f32)/2.0 - y;
-        value += ((dx*dx + dy*dy).sqrt()/29.0 + self.time).fcos();
+        value += (x/23.0 + self.time).xwave();
+        value += (x/13.0 + (y/17.0)*(self.time/2.0).xwave() ).xwave();
+        let dx = (self.time/1.9).xwave()*200.0 + (WIDTH as f32)/2.0 - x;
+        let dy = (self.time/3.1).ywave()*150.0 + (HEIGHT as f32)/2.0 - y;
+        value += ((dx*dx + dy*dy).sqrt()/29.0 + self.time).xwave();
 
         // convert to value between 0 and 1
         value = value.fract().abs();
