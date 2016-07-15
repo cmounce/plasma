6b7d04827d5022073ad74a252ca345b1192720cd
Chris Mounce <christophermounce@gmail.com>
2016-07-14 23:02:36 -0700

    Factor sine approximation into its own function

--- src/fastmath.rs
+++ src/fastmath.rs
@@ -1,58 +1,72 @@
 use std::f32;
 
 pub trait FastMath<F> {
+    fn wave(&self) -> F;
     fn xwave(&self) -> F;
     fn ywave(&self) -> F;
 }
 
 impl FastMath<f32> for f32 {
-    // Like sin(), except its period is 10 instead of 2*PI
-    fn ywave(&self) -> f32 {
-        // x loops over the range (-0.5, 0.5)
-        let adj = self/10.0;
-        let x = adj - adj.floor() - 0.5;
+    // Like sin(), except its period is 1 instead of 2*PI
+    fn wave(&self) -> f32 {
+        // x loops over the range [-0.5, 0.5).
+        // Note that x is shifted by half a period: if self is 0, x is -0.5.
+        let x = self - self.floor() - 0.5;
 
-        // f(x) = 16x^2 - 8x, a parabola centered on the interval (0.0, 0.5).
-        // Having one x and one x.abs() flips the parabola when x is negative.
+        /*
+         * Approximate a sine wave shifted by half a period.
+         *
+         *     xxx  |
+         *   xx   xx|
+         * -x-------x-------x-
+         *          |xx   xx
+         *          |  xxx
+         *
+         * The interval (0.0, 0.5) is covered by the parabola f(x) = 16x^2 - 8x.
+         * Having one x and one x.abs() flips the parabola upside-down when x is negative.
+         */
         x * x.abs().mul_add(16.0, -8.0)
     }
 
-    // Like cos(), except its period is 10 instead of 2*PI
     fn xwave(&self) -> f32 {
-        return (self + 2.5).ywave();
+        self.mul_add(0.1, 0.25).wave()
+    }
+
+    fn ywave(&self) -> f32 {
+        (self*0.1).wave()
     }
 }
 
 macro_rules! assert_feq {
-    ($expected:expr, $actual:expr) => (
+    ($a:expr, $b:expr) => (
         {
-            let expected:f32 = $expected;
-            let actual:f32 = $actual;
-            assert!(expected.abs_sub(actual) < 0.01, "assertion failed: {} != {}", expected, actual);
+            let a:f32 = $a;
+            let b:f32 = $b;
+            assert!(a.abs_sub(b) < 0.01, "assertion failed: {} != {}", a, b);
         }
     );
 }
 
 #[test]
-fn test_ywave() {
+fn test_wave() {
     // Test area around 0
-    assert_feq!((-0.5).ywave(), 0.0);
-    assert_feq!((-0.25).ywave(), -1.0);
-    assert_feq!((0.0).ywave(), 0.0);
-    assert_feq!((0.25).ywave(), 1.0);
-    assert_feq!((0.5).ywave(), 0.0);
+    assert_feq!((-0.5).wave(), 0.0);
+    assert_feq!((-0.25).wave(), -1.0);
+    assert_feq!((0.0).wave(), 0.0);
+    assert_feq!((0.25).wave(), 1.0);
+    assert_feq!((0.5).wave(), 0.0);
 
     // Test area further away
-    assert_feq!((8.0).ywave(), 0.0);
-    assert_feq!((8.25).ywave(), 1.0);
-    assert_feq!((8.5).ywave(), 0.0);
-    assert_feq!((8.75).ywave(), -1.0);
-    assert_feq!((9.0).ywave(), 0.0);
+    assert_feq!((8.0).wave(), 0.0);
+    assert_feq!((8.25).wave(), 1.0);
+    assert_feq!((8.5).wave(), 0.0);
+    assert_feq!((8.75).wave(), -1.0);
+    assert_feq!((9.0).wave(), 0.0);
 
     // Test area further into the negatives
-    assert_feq!((-8.0).ywave(), 0.0);
-    assert_feq!((-7.75).ywave(), 1.0);
-    assert_feq!((-7.5).ywave(), 0.0);
-    assert_feq!((-7.25).ywave(), -1.0);
-    assert_feq!((-7.0).ywave(), 0.0);
+    assert_feq!((-8.0).wave(), 0.0);
+    assert_feq!((-7.75).wave(), 1.0);
+    assert_feq!((-7.5).wave(), 0.0);
+    assert_feq!((-7.25).wave(), -1.0);
+    assert_feq!((-7.0).wave(), 0.0);
 }
