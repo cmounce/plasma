c8382e86b819d73c869163084ab9738287ed040b
Chris Mounce <christophermounce@gmail.com>
2016-09-01 00:25:25 -0700

    Add tests for ControlPoint::from_gene()

--- src/colormapper.rs
+++ src/colormapper.rs
@@ -32,51 +32,15 @@ impl Color {
     }
 }
 
-// TODO: Organize tests
-#[test]
-fn test_color_from_hsv() {
-    /*
-     * H: red -> green -> blue
-     * S: white -> color
-     * V: black -> color/white
-     */
-    // Test value = 0
-    assert_eq!(Color::from_hsv(0.0, 0.0, 0.0), Color::new(0, 0, 0));
-    assert_eq!(Color::from_hsv(0.0, 1.0, 0.0), Color::new(0, 0, 0));
-    assert_eq!(Color::from_hsv(0.5, 0.0, 0.0), Color::new(0, 0, 0));
-    assert_eq!(Color::from_hsv(0.5, 1.0, 0.0), Color::new(0, 0, 0));
-
-    // Test parts of color wheel
-    assert_eq!(Color::from_hsv(0.0,       1.0, 1.0), Color::new(255,   0,   0));
-    assert_eq!(Color::from_hsv(1.0/18.0,  1.0, 1.0), Color::new(255,  85,   0));
-    assert_eq!(Color::from_hsv(3.0/18.0,  1.0, 1.0), Color::new(255, 255,   0));
-    assert_eq!(Color::from_hsv(4.0/18.0,  1.0, 1.0), Color::new(170, 255,   0));
-    assert_eq!(Color::from_hsv(6.0/18.0,  1.0, 1.0), Color::new(  0, 255,   0));
-    assert_eq!(Color::from_hsv(7.0/18.0,  1.0, 1.0), Color::new(  0, 255,  85));
-    assert_eq!(Color::from_hsv(9.0/18.0,  1.0, 1.0), Color::new(  0, 255, 255));
-    assert_eq!(Color::from_hsv(10.0/18.0, 1.0, 1.0), Color::new(  0, 170, 255));
-    assert_eq!(Color::from_hsv(12.0/18.0, 1.0, 1.0), Color::new(  0,   0, 255));
-    assert_eq!(Color::from_hsv(13.0/18.0, 1.0, 1.0), Color::new( 85,   0, 255));
-    assert_eq!(Color::from_hsv(15.0/18.0, 1.0, 1.0), Color::new(255,   0, 255));
-    assert_eq!(Color::from_hsv(16.0/18.0, 1.0, 1.0), Color::new(255,   0, 170));
-    assert_eq!(Color::from_hsv(1.0,       1.0, 1.0), Color::new(255,   0,   0));
-
-    // Test saturation
-    assert_eq!(Color::from_hsv(0.0, 1.0, 1.0),      Color::new(255, 0, 0));
-    assert_eq!(Color::from_hsv(0.0, 2.0/3.0, 1.0),  Color::new(255, 85, 85));
-    assert_eq!(Color::from_hsv(0.0, 1.0/3.0, 1.0),  Color::new(255, 170, 170));
-}
-
 impl ControlPoint {
-    // TODO: write tests
     fn from_gene(gene: &Gene) -> Option<ControlPoint> {
         assert!(gene.data.len() == 5);
         let activation_threshold = 160;
         if gene.data[0] > activation_threshold {
-            let h = (gene.data[1] as f32)/256.0; // allow h = 1.0
-            let s = (gene.data[2] as f32)/256.0; // allow s = 1.0
-            let v = (gene.data[3] as f32)/256.0; // allow v = 1.0
-            let position = (gene.data[4] as f32)/255.0; // disallow position = 1.0
+            let h = (gene.data[1] as f32)/256.0; // disallow h = 1.0 (wraps to 0.0)
+            let s = (gene.data[2] as f32)/255.0; // allow s = 1.0
+            let v = (gene.data[3] as f32)/255.0; // allow v = 1.0
+            let position = (gene.data[4] as f32)/256.0; // disallow position = 1.0 (wraps to 0.0)
             Some(ControlPoint {
                 color: Color::from_hsv(h, s, v),
                 position: position
@@ -121,5 +85,88 @@ impl ColorMapper {
         let index = (value.wrap()*(LOOKUP_TABLE_SIZE as f32)).floor() as usize % LOOKUP_TABLE_SIZE;
         let color = self.lookup_table[index];
         (color.r, color.g, color.b)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use genetics::Gene;
+    use gradient::Color;
+    use gradient::ControlPoint;
+
+    #[test]
+    fn test_color_from_hsv() {
+        /*
+         * H: red -> green -> blue
+         * S: white -> color
+         * V: black -> color/white
+         */
+        // Test value = 0
+        assert_eq!(Color::from_hsv(0.0, 0.0, 0.0), Color::new(0, 0, 0));
+        assert_eq!(Color::from_hsv(0.0, 1.0, 0.0), Color::new(0, 0, 0));
+        assert_eq!(Color::from_hsv(0.5, 0.0, 0.0), Color::new(0, 0, 0));
+        assert_eq!(Color::from_hsv(0.5, 1.0, 0.0), Color::new(0, 0, 0));
+
+        // Test parts of color wheel
+        assert_eq!(Color::from_hsv(0.0,       1.0, 1.0), Color::new(255,   0,   0));
+        assert_eq!(Color::from_hsv(1.0/18.0,  1.0, 1.0), Color::new(255,  85,   0));
+        assert_eq!(Color::from_hsv(3.0/18.0,  1.0, 1.0), Color::new(255, 255,   0));
+        assert_eq!(Color::from_hsv(4.0/18.0,  1.0, 1.0), Color::new(170, 255,   0));
+        assert_eq!(Color::from_hsv(6.0/18.0,  1.0, 1.0), Color::new(  0, 255,   0));
+        assert_eq!(Color::from_hsv(7.0/18.0,  1.0, 1.0), Color::new(  0, 255,  85));
+        assert_eq!(Color::from_hsv(9.0/18.0,  1.0, 1.0), Color::new(  0, 255, 255));
+        assert_eq!(Color::from_hsv(10.0/18.0, 1.0, 1.0), Color::new(  0, 170, 255));
+        assert_eq!(Color::from_hsv(12.0/18.0, 1.0, 1.0), Color::new(  0,   0, 255));
+        assert_eq!(Color::from_hsv(13.0/18.0, 1.0, 1.0), Color::new( 85,   0, 255));
+        assert_eq!(Color::from_hsv(15.0/18.0, 1.0, 1.0), Color::new(255,   0, 255));
+        assert_eq!(Color::from_hsv(16.0/18.0, 1.0, 1.0), Color::new(255,   0, 170));
+        assert_eq!(Color::from_hsv(1.0,       1.0, 1.0), Color::new(255,   0,   0));
+
+        // Test saturation
+        assert_eq!(Color::from_hsv(0.0, 1.0, 1.0),      Color::new(255, 0, 0));
+        assert_eq!(Color::from_hsv(0.0, 2.0/3.0, 1.0),  Color::new(255, 85, 85));
+        assert_eq!(Color::from_hsv(0.0, 1.0/3.0, 1.0),  Color::new(255, 170, 170));
+    }
+
+    // Make sure max/min byte values map to different hues
+    #[test]
+    fn test_from_gene_hue() {
+        let g1 = Gene { data: vec![255, 255, 255, 255, 255] };
+        let g2 = Gene { data: vec![255,   0, 255, 255, 255] };
+        let cp1 = ControlPoint::from_gene(&g1).unwrap();
+        let cp2 = ControlPoint::from_gene(&g2).unwrap();
+        assert!(cp1.color != cp2.color); // Make sure we have different hues
+    }
+
+    // Make sure full range of saturation is possible
+    #[test]
+    fn test_from_gene_saturation() {
+        let g1 = Gene { data: vec![255, 0, 255, 255, 255] };
+        let g2 = Gene { data: vec![255, 0,   0, 255, 255] };
+        let cp1 = ControlPoint::from_gene(&g1).unwrap();
+        let cp2 = ControlPoint::from_gene(&g2).unwrap();
+        assert_eq!(cp1.color, Color::from_hsv(0.0, 1.0, 1.0));
+        assert_eq!(cp2.color, Color::from_hsv(0.0, 0.0, 1.0));
+    }
+
+    // Make sure full range of value is possible
+    #[test]
+    fn test_from_gene_value() {
+        let g1 = Gene { data: vec![255, 0, 255, 255, 255] };
+        let g2 = Gene { data: vec![255, 0, 255,   0, 255] };
+        let cp1 = ControlPoint::from_gene(&g1).unwrap();
+        let cp2 = ControlPoint::from_gene(&g2).unwrap();
+        assert_eq!(cp1.color, Color::from_hsv(0.0, 1.0, 1.0));
+        assert_eq!(cp2.color, Color::from_hsv(0.0, 1.0, 0.0));
+    }
+
+    // Make sure max/min byte values map to different positions
+    #[test]
+    fn test_from_gene_position() {
+        let g1 = Gene { data: vec![255, 255, 255, 255, 255] };
+        let g2 = Gene { data: vec![255, 255, 255, 255,   0] };
+        let cp1 = ControlPoint::from_gene(&g1).unwrap();
+        let cp2 = ControlPoint::from_gene(&g2).unwrap();
+        assert!(cp1.position != cp2.position);
     }
 }
