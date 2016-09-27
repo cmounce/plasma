8a66367d0e03cc70273ad95a55f81923159f2d2e
Chris Mounce <christophermounce@gmail.com>
2016-09-27 00:16:02 -0700

    Add a variable wave amplitude to each plasma formula

--- src/formulas.rs
+++ src/formulas.rs
@@ -1,11 +1,12 @@
 use genetics::{Gene, Chromosome};
 use fastmath::FastMath;
 
-pub const FORMULA_GENE_SIZE: usize = 4;
+pub const FORMULA_GENE_SIZE: usize = 5;
 pub const NUM_FORMULA_GENES: usize = 3;
 
 // TODO: Figure out how to store precomputed values
 struct WaveFormula {
+    amplitude: f32,
     x_scale: f32,
     y_scale: f32,
     scale: f32,
@@ -13,6 +14,7 @@ struct WaveFormula {
 }
 
 struct RotatingWaveFormula {
+    amplitude: f32,
     x_time: f32,
     y_time: f32,
     scale: f32,
@@ -20,6 +22,7 @@ struct RotatingWaveFormula {
 }
 
 struct CircularWaveFormula {
+    amplitude: f32,
     x_time: f32,
     y_time: f32,
     scale: f32,
@@ -42,12 +45,13 @@ fn byte_to_ifloat(byte: u8) -> f32 {
 
 impl WaveFormula {
     fn from_gene(gene: &Gene) -> WaveFormula {
-        assert!(gene.data.len() == 4);
+        assert!(gene.data.len() == FORMULA_GENE_SIZE);
         WaveFormula {
-            x_scale: byte_to_float(gene.data[0]),
-            y_scale: byte_to_float(gene.data[1]),
-            scale: byte_to_float(gene.data[2]),
-            wave_speed: byte_to_ifloat(gene.data[3])
+            amplitude: byte_to_float(gene.data[0]),
+            x_scale: byte_to_float(gene.data[1]),
+            y_scale: byte_to_float(gene.data[2]),
+            scale: byte_to_float(gene.data[3]),
+            wave_speed: byte_to_ifloat(gene.data[4])
         }
     }
 
@@ -55,18 +59,19 @@ impl WaveFormula {
     pub fn get_value(&self, x: f32, y: f32, time: f32) -> f32 {
         let x_factor = self.x_scale.cowave();
         let y_factor = self.y_scale.wave();
-        (self.scale*(x*x_factor + y*y_factor) + self.wave_speed*time).wave()
+        (self.scale*(x*x_factor + y*y_factor) + self.wave_speed*time).wave()*self.amplitude
     }
 }
 
 impl RotatingWaveFormula {
     fn from_gene(gene: &Gene) -> RotatingWaveFormula {
-        assert!(gene.data.len() == 4);
+        assert!(gene.data.len() == FORMULA_GENE_SIZE);
         RotatingWaveFormula {
-            x_time: byte_to_ifloat(gene.data[0]),
-            y_time: byte_to_ifloat(gene.data[1]),
-            scale: byte_to_float(gene.data[2]),
-            wave_speed: byte_to_ifloat(gene.data[3])
+            amplitude: byte_to_float(gene.data[0]),
+            x_time: byte_to_ifloat(gene.data[1]),
+            y_time: byte_to_ifloat(gene.data[2]),
+            scale: byte_to_float(gene.data[3]),
+            wave_speed: byte_to_ifloat(gene.data[4])
         }
     }
 
@@ -74,7 +79,7 @@ impl RotatingWaveFormula {
     pub fn get_value(&self, x: f32, y: f32, time: f32) -> f32 {
         let x_factor = (self.x_time*time).cowave();
         let y_factor = (self.y_time*time).wave();
-        (self.scale*(x*x_factor + y*y_factor) + self.wave_speed*time).wave()
+        (self.scale*(x*x_factor + y*y_factor) + self.wave_speed*time).wave()*self.amplitude
     }
 }
 
@@ -82,10 +87,11 @@ impl CircularWaveFormula {
     fn from_gene(gene: &Gene) -> CircularWaveFormula {
         assert!(gene.data.len() == FORMULA_GENE_SIZE);
         CircularWaveFormula {
-            x_time: byte_to_ifloat(gene.data[0]),
-            y_time: byte_to_ifloat(gene.data[1]),
-            scale: byte_to_float(gene.data[2]),
-            wave_speed: byte_to_ifloat(gene.data[3])
+            amplitude: byte_to_float(gene.data[0]),
+            x_time: byte_to_ifloat(gene.data[1]),
+            y_time: byte_to_ifloat(gene.data[2]),
+            scale: byte_to_float(gene.data[3]),
+            wave_speed: byte_to_ifloat(gene.data[4])
         }
     }
 
@@ -93,7 +99,7 @@ impl CircularWaveFormula {
     pub fn get_value(&self, x: f32, y: f32, time: f32) -> f32 {
         let dx = x - (self.x_time*time).cowave();
         let dy = y - (self.y_time*time).wave();
-        (self.scale*(dx*dx + dy*dy + 0.1).sqrt() + self.wave_speed*time).wave()
+        (self.scale*(dx*dx + dy*dy + 0.1).sqrt() + self.wave_speed*time).wave()*self.amplitude
     }
 }
 
@@ -117,20 +123,19 @@ impl PlasmaFormulas {
 #[cfg(test)]
 mod tests {
     use genetics::Gene;
-    use super::WaveFormula;
-    use super::RotatingWaveFormula;
-    use super::CircularWaveFormula;
+    use super::FORMULA_GENE_SIZE;
+    use super::{WaveFormula,RotatingWaveFormula,CircularWaveFormula};
 
     #[test]
     fn test_wave_formula_from_gene() {
-        let g = Gene::rand(4);
+        let g = Gene::rand(FORMULA_GENE_SIZE);
         let wf = WaveFormula::from_gene(&g);
         assert!(wf.wave_speed.fract() == 0.0);
     }
 
     #[test]
     fn test_rotating_wave_formula_from_gene() {
-        let g = Gene::rand(4);
+        let g = Gene::rand(FORMULA_GENE_SIZE);
         let wf = RotatingWaveFormula::from_gene(&g);
         assert!(wf.x_time.fract() == 0.0);
         assert!(wf.y_time.fract() == 0.0);
@@ -139,7 +144,7 @@ mod tests {
 
     #[test]
     fn test_circular_wave_formula_from_gene() {
-        let g = Gene::rand(4);
+        let g = Gene::rand(FORMULA_GENE_SIZE);
         let cf = CircularWaveFormula::from_gene(&g);
         assert!(cf.x_time.fract() == 0.0);
         assert!(cf.y_time.fract() == 0.0);
