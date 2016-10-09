adf8fee8b23423cef258c8e2d72011f8781b58cd
Chris Mounce <christophermounce@gmail.com>
2016-10-08 23:40:32 -0700

    Change FPS from 10 to 16, clean up FPS code

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ mod plasma;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::event::WindowEventId;
-use std::cmp;
 use std::f32;
 use std::time::SystemTime;
 use plasma::Plasma;
+
+const FRAMES_PER_SECOND: f32 = 16.0;
 
 // Default window size
 const WIDTH: u32 = 640;
@@ -63,18 +64,22 @@ fn main() {
         }
         plasma.update(&mut renderer);
 
-        // Manage time
+        // Sleep to hit framerate
         let duration = timestamp.elapsed().unwrap();
-        let target_ms = 100;
-        let actual_ms = (duration.subsec_nanos() as u64)/1000000 + duration.as_secs()*1000;
-        avg_render_time += actual_ms as f32;
-        avg_render_time_count += 1;
+        let target_ms = 1000.0/FRAMES_PER_SECOND;
+        let actual_ms = duration.subsec_nanos() as f32/1_000_000.0 +
+            duration.as_secs() as f32*1000.0;
         if actual_ms > target_ms {
             println!("Target frame delay is {} but actual time taken is {}", target_ms, actual_ms);
         } else {
-            std::thread::sleep(std::time::Duration::from_millis(target_ms - actual_ms));
+            let sleep_ms = (target_ms - actual_ms).max(0.0) as u64;
+            std::thread::sleep(std::time::Duration::from_millis(sleep_ms));
         }
-        plasma.add_time((cmp::max(target_ms, actual_ms) as f32)/1000.0);
+        plasma.add_time(target_ms.max(actual_ms)/1000.0);
+
+        // Calculate time statistics
+        avg_render_time += actual_ms;
+        avg_render_time_count += 1;
         if avg_render_time_count >= 50 {
             println!("Average render time: {} ms", avg_render_time/(avg_render_time_count as f32));
             avg_render_time = 0.0;
